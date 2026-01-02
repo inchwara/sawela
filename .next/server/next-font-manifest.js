48ea6605166f83688af5db6793024e91e4355ddd
Samuel Inchwara <inchwara@gmail.com>
2026-01-02 10:39:43 +0300

    chore: update puppeteer dependency to version 24.34.0

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/inchwara/sawela/app/layout\":[\"static/media/de42cfb9a3b980ae-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/inchwara/sawela/app/layout":["static/media/de42cfb9a3b980ae-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
