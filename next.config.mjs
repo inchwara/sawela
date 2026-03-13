d839095619a9050a9407a6e182ae4bd3b0ff4b87
Samuel Inchwara <inchwara@gmail.com>
2026-03-13 16:05:55 +0300

    Image upload

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**',
       },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
     ],
   },
   typescript: {
